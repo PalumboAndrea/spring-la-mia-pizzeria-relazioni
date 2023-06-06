f3139fcbd306613c11ce39cfeaeae692b8787ef5
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-06-06 15:49:38 +0200

    login

--- src/main/java/com/example/demo/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/com/example/demo/SpringLaMiaPizzeriaCrudApplication.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.demo.pojo.OffertaSpeciale;
 import com.example.demo.pojo.Pizza;
+import com.example.demo.auth.Serv.RoleService;
+import com.example.demo.auth.Serv.UserService;
 import com.example.demo.pojo.Ingrediente;
 import com.example.demo.serv.IngredienteServ;
 import com.example.demo.serv.OffertaSpecialeServ;
 import com.example.demo.serv.PizzaService;
+import com.example.demo.auth.pojo.Role;
+import com.example.demo.auth.pojo.User;
 
 @SpringBootApplication
 public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
@@ -31,9 +36,29 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
 	@Autowired
 	private IngredienteServ ingredienteServ;
 	
+	@Autowired
+	private RoleService roleService;
+	
+	@Autowired
+	private UserService userService;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		Role roleUser = new Role("USER");
+		Role roleAdmin = new Role("ADMIN");
+		
+		roleService.save(roleUser);
+		roleService.save(roleAdmin);
+		
+		final String pws = new BCryptPasswordEncoder().encode("pws");
+		
+		User userUser = new User("user", pws, roleUser);
+		User userAdmin = new User("admin", pws, roleAdmin);
+				
+		userService.save(userUser);
+		userService.save(userAdmin);
 		
 		Ingrediente ingrediente1 = new Ingrediente("Peperoncino");
 		Ingrediente ingrediente2 = new Ingrediente("Pomodori");
